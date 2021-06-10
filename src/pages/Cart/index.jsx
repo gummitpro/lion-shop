fe71ef29aab16fe0454c6ce6e34739f73125fd94
= <=>
2021-06-10 19:20:09 +0700

    cmm2

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,82 +1,176 @@
 import { Row, Col } from 'antd';
+import { connect } from 'react-redux';
 import Header from "../commom/Header";
 import Footer from "../commom/Footer";
+import history from '../../utils/history';
 import "./style.css";
-function index() {
-  var localStorageCart = JSON.parse(localStorage.getItem("shoppingCart"));
-  console.log("localStorageCart", localStorageCart.length)
-  const renderListCart =  localStorageCart.map((item) => {
+
+import { incProductCartAction, decProductCartAction, removeProductCartAction } from '../../redux/actions';
+
+function index({userInfo, shoppingCart, decCart, incCart, removeCart }) {
+  var localStorageCart = shoppingCart.data;
+  var totalMoney = 0;
+  const renderListCart = localStorageCart.map((item, index) => {
+    totalMoney += item.price * item.quantity
     return (
-      <div className="cart-product">
-        <div className="cart-image"  height="80" width="80">
-          <img className="lazyload" alt="product" src={item.image} loading="lazy" decoding="async"/>
+      <div className="cart-product" key={index} >
+        <div className="cart-product-left">
+        <div className="cart-image" height="80" width="80">
+          <img className="lazyload" alt="product" src={item.image} loading="lazy" decoding="async" />
         </div>
-        <div className="cart-name" style={{width: "300px"}}>
-          <p>{item.name}</p>
+        <div className="cart-name" >
+          <p>{item.name} {item.productOptions && item.productOptions.memory}</p>
         </div>
-        <div className="cart-quanty">
-          
-          <button className="btn-quantity">
-            {/* <span >{item.quantity === 1 ? (<i className="fal fa-trash-alt"></i>) : "-"}</span> */}
-            <span>-</span>
-          </button>
-          <input type="number" className="content-quantity" value={item.quantity} readOnly style={{marginLeft: "10px"}} />
-          <button className="btn-quantity">
-            <span>+</span>
-          </button>
-          
         </div>
-        <div className="cart-price">
-          <p>{item.price.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</p>
+        <div className="cart-action">
+          <div className="cart-quanty">
+            <button className="btn-quantity" onClick={() => { decCart({id: item.id, productOptions:item.productOptions}) }}>
+              <span>-</span>
+            </button>
+            <input type="number" className="content-quantity" value={item.quantity} readOnly style={{ marginLeft: "10px" }} />
+            <button className="btn-quantity" onClick={() => { incCart({id: item.id, productOptions:item.productOptions}) }}>
+              <span>+</span>
+            </button>
+
+          </div>
+          <div className="cart-price">
+            <p>{Object.keys(item.productOptions).length > 0 ? (item.price + item.productOptions.price).toLocaleString('vi', { style: 'currency', currency: 'VND' }) : item.price.toLocaleString('vi', { style: 'currency', currency: 'VND' }) }</p>
+          </div>
+          <div className="cart-remove">
+            <button className="btn-remove" onClick={() => { removeCart({id: item.id, productOptions:item.productOptions}) }}>
+              <i className="fal fa-trash-alt"></i>
+            </button>
+          </div>
         </div>
+
       </div>
     )
   })
+
+  const renderListCartMoblie = localStorageCart.map((item, index) => {
+    totalMoney += item.price * item.quantity
+    return (
+      <div className="cart-product-mobile" key={index} >
+        <div className="cart-product-left-mobile">
+          <div className="cart-image" height="80" width="80">
+            <img className="lazyload" alt="product" src={item.image} loading="lazy" decoding="async" />
+          </div>
+        </div>
+        <div className="cart-product-right-mobile">
+          <div className="cart-product-right-content">
+            <div className="cart-name-moblie" >
+              <p>{item.name} {item.productOptions && item.productOptions.memory}</p>
+            </div>
+            <div className="cart-remove">
+              <button className="btn-remove" onClick={() => { removeCart({id: item.id, productOptions:item.productOptions}) }}>
+                <i className="fal fa-trash-alt"></i>
+              </button>
+            </div>
+          </div>
+          <div className="cart-product-right-content">
+            <div className="cart-price-moblie">
+              <p>{Object.keys(item.productOptions).length > 0 ? (item.price + item.productOptions.price).toLocaleString('vi', { style: 'currency', currency: 'VND' }) : item.price.toLocaleString('vi', { style: 'currency', currency: 'VND' }) }</p>
+            </div>
+            <div className="cart-quanty">
+              <button className="btn-quantity" onClick={() => { decCart({id: item.id, productOptions:item.productOptions}) }}>
+                <span>-</span>
+              </button>
+              <input type="number" className="content-quantity" value={item.quantity} readOnly style={{ marginLeft: "10px" }} />
+              <button className="btn-quantity" onClick={() => { incCart({id: item.id, productOptions:item.productOptions}) }}>
+                <span>+</span>
+              </button>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    )
+  })
+
   return (
     <>
       <Header />
-      <div className="wrap-cart">
-        <Row justify='center' className="content-cart" gutter={[16, 16]}>
-          <Col span={16}>
-            <h2>Giỏ hàng của bạn</h2>
-            <div className="list-cart-product">
-              
-              {renderListCart}
 
-            </div>
-          </Col>
-          <Col span={8}>
-            <h2>Thanh toán</h2>
-            <div className="content-payment">
-             
-              <div className="info-payment">
-                <div>
-                  <p>Tạm tính</p>
-                  <p>123100000</p>
+      <div className="wrap-cart">
+        {localStorageCart.length > 0 ? (
+          <Row justify='center' className="content-cart" gutter={[16, 16]}>
+            <Col span={16} md={16} sm={24} xs={24}>
+              <h2>Giỏ hàng của bạn</h2>
+              <div className="list-cart-product">
+                {renderListCart}
+              </div>
+              <div className="list-cart-product-mobile">
+                {renderListCartMoblie}
+              </div>
+            </Col>
+            <Col span={8} md={8} sm={24} xs={24}>
+              <h2>Thanh toán</h2>
+              <div className="content-payment">
+                <div className="info-payment">
+                  <div>
+                    <p>Tạm tính</p>
+                    <p>{totalMoney.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</p>
+                  </div>
+                  <div>
+                    <p>Phí vận chuyển</p>
+                    <p>{(50000).toLocaleString('vi', { style: 'currency', currency: 'VND' })}</p>
+                  </div>
+                  <div>
+                    <p>Thành tiền</p>
+                    <p className="sumary-money">{(totalMoney + 50000).toLocaleString('vi', { style: 'currency', currency: 'VND' })}</p>
+                  </div>
+                  <button className="btn-payment" onClick={() => {
+                    if(!userInfo.data.id){
+                      return history.push({
+                        pathname: "/dang-nhap",
+                        state: {
+                          prevPath: "/gio-hang"
+                        }
+                      })
+                    }else{
+                      return history.push(`/thanh-toan`)
+                    }
+                  }}>
+                    Thanh toán {!userInfo.data.id && (<p style={{fontSize:"12px", marginBottom: "0px"}}>Vui lòng đăng nhâp</p>)}
+                  </button>
                 </div>
-                <div>
-                  <p>Phí vận chuyển</p>
-                  <p>123200000</p>
-                </div>
-                <div>
-                  <p>Khuyến mãi</p>
-                  <p>123200000</p>
-                </div>
-                <div>
-                  <p>Thành tiền</p>
-                  <p className="sumary-money">123200000</p>
-                </div>
-                <button className="btn-payment">
-                  Thanh toán ngay
-                </button>
+              </div>
+            </Col>
+          </Row>
+        ) : (
+          <div className="content-cart-empty">
+            <div className="content-image">
+              <div className="image-empty">
+                <img src="https://i.imgur.com/Drj57qu.png" alt="" />
               </div>
             </div>
-          </Col>
-        </Row>
+            <p style={{ textAlign: "center" }}>Giỏ hàng chưa có sản phẩm nào</p>
+            <button className="btn-payment" onClick={() => history.push(`/`)}>Mua sắm ngay</button>
+          </div>
+
+        )}
+
       </div>
       <Footer />
     </>
   )
 }
+const mapStateToProps = (state) => {
+  const { userInfo } = state.userReducer;
+  const { shoppingCart } = state.productReducer;
+  return {
+    shoppingCart,
+    userInfo
+  }
+};
 
-export default index;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    decCart: (params) => dispatch(decProductCartAction(params)),
+    incCart: (params) => dispatch(incProductCartAction(params)),
+    removeCart: (params) => dispatch(removeProductCartAction(params))
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(index);

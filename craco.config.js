df5178bdbff37514f0d03d1e9b6f9b6e578bdf40
gummitpro <quanglinhevt@gmail.com>
2021-06-11 17:19:00 +0700

    cmm9

--- craco.config.js
+++ craco.config.js
@@ -8,8 +8,12 @@ module.exports = {
         lessLoaderOptions: {
           lessOptions: {
             modifyVars: { 
-              '@primary-color': '#1DA57A', 
-              '@border-radius-base': '8px'
+              '@primary-color': '#13c2c2',
+              '@border-radius-base': '8px',
+              '@table-header-bg': '#ff7a45',
+              '@table-header-color': '#fff2e8',
+              '@menu-highlight-color': '#ff7a45',
+              '@menu-item-active-bg': '#ffbb96',
             },
             javascriptEnabled: true,
           },
